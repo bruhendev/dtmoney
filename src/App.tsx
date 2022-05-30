41f3aee2043cc8f890f5352b03f91542d6c1a0cd
Bruno <brunohenrique242@gmail.com>
2022-05-30 12:35:52 -0300

    Iniciando o Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { Dashborad } from "./components/Dashboard";
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+      <Header />
+      <Dashborad />
       <GlobalStyle/>
-    </div>
+    </>
   );
 }
---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Summary } from "../Summary";
+import { Container } from "./syles";
+
+export function Dashborad() {
+    return (
+        <Container>
+            <Summary></Summary>
+        </Container>
+    );
+}
