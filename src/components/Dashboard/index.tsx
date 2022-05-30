2dd342e10a5104932c2d1dc864ed712e0a599961
Bruno Henrique <brunohenrique242@gmail.com>
2022-05-30 15:04:09 -0300

    Criado o componente TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./syles";
 
 export function Dashborad() {
     return (
         <Container>
             <Summary></Summary>
+            <TransactionsTable />
         </Container>
     );
 }
